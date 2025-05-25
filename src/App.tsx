124d551d5b09b00b4bc60c1e4283cf99fb4bb3ec
とっきー <kaonasi201@gmail.com>
2025-05-25 21:43:30 +0900

    Move titlebar to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import BackgroundImage from "./assets/temp.jpg"
 import "./App.css";
 
 function App() {
+  const appWindow = getCurrentWindow();
+  const closeWindow = async () => {
+    try {
+      appWindow.close()
+    } catch (error) {
+      console.error('Failed to close window:', error)
+    }
+  }
   const handleMaximize = async () => {
     try {
-      const appWindow = getCurrentWindow();
       await appWindow.toggleMaximize();
     } catch (error) {
       console.error('Failed to maximize window:', error);
@@ -15,6 +22,12 @@ function App() {
 
   return (
     <main className="container">
+      <div data-tauri-drag-region className="titlebar">
+        <div className="titlebar-button" id="titlebar-close" onClick={closeWindow}>
+          <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+        </div>
+      </div>
+
       <div className="background-props">
         <img src={BackgroundImage} className="background-image" />
       </div>
